27373765399fb919f406649c2a9591f21ea21b37
wzl <wzl15686266108@163.com>
2020-07-03 17:28:19 +0800

    first change

---
+++ FCD_main.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+from pylab import mpl  
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+import numpy as np
+import tensorflow as tf
+from matplotlib import pyplot as plt
+import h5py
+hidden1_size=32 #lstm中隐藏节点的个数
+hidden2_size=40
+
+dnn_size=10
+num_layers =2#LSTM层数
+timesteps=256
+training_steps=2000#训练轮数
+batch_size=32 #batch大小
+
+training_examples=5500#训练数据个数
+testing_examples=508#测试数据个数
+
+trainpath="traindata.mat"
+testpath="testdata.mat"
+
+def read_datat(trainpath,testpath):
+    traindata=h5py.File(trainpath)
+    testdata=h5py.File(testpath)
+
+    train_sig=np.array(traindata["train_sig"],dtype=np.float32).T
+    train_sig=np.expand_dims(train_sig,1)
+    train_lab = np.array(traindata["train_lab"], dtype=np.float32).T
+
+    test_sig=np.array(testdata["test_sig"],dtype=np.float32).T
+    test_sig=np.expand_dims(test_sig,1)
+    test_lab = np.array(testdata["test_lab"], dtype=np.float32).T
+
+    return train_sig,train_lab,test_sig,test_lab
+
+def fully_connected(prev_layer,num_units,is_training):
+    layer=tf.layers.batch_normalization(prev_layer,training=is_training)
+    layer=tf.layers.dense(layer,num_units,use_bias=False,activation=None)
+    layer = tf.layers.batch_normalization(layer, training=is_training)
+    layer=tf.nn.tanh(layer)
+    output=tf.layers.dense(layer,1,use_bias=False,activation=tf.nn.sigmoid)
+    return output
+
+def lstm_model(X,y,is_training):
+    cell=tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.BasicLSTMCell(hidden1_size),tf.nn.rnn_cell.BasicLSTMCell(hidden2_size)])
+    outputs,_=tf.nn.dynamic_rnn(cell,X,dtype=tf.float32)
+    output=outputs[:,-1,:]
+    predictions=fully_connected(output,dnn_size,is_training)
+    if not is_training:
+        return predictions,None,None
+    loss=tf.losses.mean_squared_error(labels=y,predictions=predictions)
+    with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+        train_op=tf.contrib.layers.optimize_loss(loss,tf.train.get_global_step(),optimizer="Adagrad",learning_rate=0.1)
+    return predictions,loss,train_op
+
+def train(sess,train_X,train_y):
+    ds=tf.data.Dataset.from_tensor_slices((train_X,train_y))
+    ds=ds.repeat().shuffle(1000).batch(batch_size)#repeat:遍历数据集次数 shuffle:混乱程度
+    X,y=ds.make_one_shot_iterator().get_next()
+
+    #调用模型，得到预测结果
+    with tf.variable_scope("model"):
+        predictions,loss,train_op=lstm_model(X,y,True)
+
+    #初始化变量
+    sess.run(tf.global_variables_initializer())
+    for i in range(training_steps):
+        _,l=sess.run([train_op,loss])#使用dataset而不是placeholder导入数据
+        if i%100==0:
+            print("train step:"+str(i)+",loss"+str(l))
+def run_eval(sess,test_X,test_y):
+    ds=tf.data.Dataset.from_tensor_slices((test_X,test_y))
+    ds=ds.batch(1)
+    X,y=ds.make_one_shot_iterator().get_next()
+    with tf.variable_scope("model",reuse=True):#reuse训练时的变量
+        prediction,_,_=lstm_model(X,[0.0],False)
+        predictions=[]
+        labels=[]
+        for i in range(testing_examples):
+            p,l=sess.run([prediction,y])
+            predictions.append(p)
+            labels.append(l)
+        predictions=np.array(predictions).squeeze()#2维变一维
+        labels=np.array(labels).squeeze()
+        rmse=np.sqrt(((predictions-labels)**2).mean(axis=0))
+        print("MSE is :%f" % rmse)
+
+        #画图
+        plt.figure()
+        plt.xlabel('时间')
+        plt.ylabel('跳变点概率')
+        plt.plot(predictions,label='predictions')
+        plt.plot(labels,label='real')
+        plt.legend()
+        plt.show()
+
+if __name__ == '__main__':
+    train_X, train_y, test_X, test_y = read_datat(trainpath, testpath)
+
+    with tf.Session() as sess:
+        #训练模型
+        train(sess,train_X,train_y)
+        #进行预测
+        run_eval(sess,test_X,test_y)
---
+++ datagen.m
@@ -0,0 +1,93 @@
+clc
+clear all
+N_t=256;%滑动窗长
+fre_N=200;%频率集点数
+freset=10e5:(10e5/fre_N):20e5;%频率集
+fs=40e5;%采样频率
+
+fm=1e3;%调制频率
+train_num=5000;
+test_num=500;
+%% 构造训练数据集
+train_lab=[];
+train_sig=[];
+tao=10;
+show=1;
+for n=1:train_num
+    %构造跳变数据
+    split_posi=randi([2,254],1);%跳频点
+    tl=(0:split_posi-1)*(1/fs);%l左
+    tr=(split_posi:N_t-1)*(1/fs);%r右
+
+    f=randperm(fre_N, 2);%随机在频率集里选择两个频率
+    
+    cur_label=max(0,1-abs(split_posi-N_t/2)/tao);
+    train_lab=[train_lab;cur_label];
+    signall=exp(1i*2*pi*(freset(f(1))+randi([1,4],1)*fm)*tl);%构造信号
+    signalr=exp(1i*2*pi*(freset(f(2))+randi([1,4],1)*fm)*tr);
+    cur_signal=[signall signalr];
+    spec=abs(fft(cur_signal,N_t));
+    spec=mapminmax(spec,0,1);
+    train_sig=[train_sig;spec];
+
+    %%画图
+    if show==1
+       freset(f(1))
+       freset(f(2))
+       ff=(0:N_t-1)*(fs/N_t);
+       figure()
+       plot(ff,spec);
+       show=0;
+    end
+end
+%构造单频数据
+for n=1:train_num/10
+    t=(0:N_t-1)*(1/fs);
+    ff=randperm(fre_N, 1);
+    train_lab=[train_lab;0];
+    signal=exp(1i*2*pi*(freset(ff)+randi([1,4],1)*fm)*t);
+    spec=abs(fft(signal,N_t));
+    spec=mapminmax(spec,0,1);%归一化
+    train_sig=[train_sig;spec];
+end
+save('traindata.mat','train_sig','train_lab')
+%% 构造测试数据
+test_fre=[12e5,18e5,13e5];
+test_lab=[];
+test_sig=[];
+for n=1:2
+    %单频
+    test_lab=[test_lab;0];
+    signal=exp(1i*2*pi*(test_fre(n)+randi([1,4],1)*fm)*t);
+    spec=abs(fft(signal,N_t));
+    spec=mapminmax(spec,0,1);
+    test_sig=[test_sig;spec];
+    %跳频
+    for hop=2:N_t-2
+        split_posi=hop;%跳频点
+        tl=(0:split_posi-1)*(1/fs);
+        tr=(split_posi:N_t-1)*(1/fs);
+        
+        cur_label=max(0,1-abs(split_posi-N_t/2)/tao);
+        test_lab=[test_lab;cur_label];
+        
+        signall=exp(1i*2*pi*(test_fre(n)+randi([1,4],1)*fm)*tl);%构造信号
+        signalr=exp(1i*2*pi*(test_fre(n+1)+randi([1,4],1)*fm)*tr);
+        
+        cur_signal=[signall signalr];
+        spec=abs(fft(cur_signal,N_t));
+        spec=mapminmax(spec,0,1);
+        
+        test_sig=[test_sig;spec];
+        if hop==128
+           ff=(0:N_t-1)*(fs/N_t);
+           figure()
+           plot(ff,spec);
+        end
+    end
+end
+save('testdata.mat','test_sig','test_lab')
+
+
+
+
